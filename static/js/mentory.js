189c3c0b4c55d49bfa6c648c2f0c1f9d48be050f
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-02-19 13:08:14 +0100

    CHECK SOLUTION corrected

--- static/js/mentory.js
+++ static/js/mentory.js
@@ -7,14 +7,16 @@ function checkSolution() {
 
 function getAllImages() {
     let imgs = [];
-    for (i; i <= 12; i++) {
+    for (i = 1; i <= 12; i++) {
         if (i < 10) {
-            let id = "game-field0".concat(i);
+            var id = "game-field0".concat(i);
         } else {
-            let id = "game-field".concat(i);
+            var id = "game-field".concat(i);
         }
         img = document.getElementById(id);
-        id.push(imgs);
+        imgs.push(img);
     }
     return imgs;
 }
+
+checkSolution();
