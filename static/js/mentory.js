3dbb3470705df077987b17e375c761e9c56a6811
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-02-20 10:15:51 +0100

    CHECK SOLUTION on my way...

--- static/js/mentory.js
+++ static/js/mentory.js
@@ -1,22 +1,31 @@
-function checkSolution() {
-    let imgs = getAllImages();
-    for (img of imgs) {
-        console.log("Hurray!");
-    }
-}
+var solution = ["img-01", "img-02"];
 
-function getAllImages() {
-    let imgs = [];
-    for (i = 1; i <= 12; i++) {
-        if (i < 10) {
-            var id = "game-field0".concat(i);
-        } else {
-            var id = "game-field".concat(i);
+function checkSolution(solution) {
+    let imageIdsOnGameField = getAllImageIdsFromGameField();
+    for (index = 0; index < solution.length; index++) {
+        if (imageIdsOnGameField[index] != solution[index]) {
+            alert("Wrong solution");
+            return
         }
-        img = document.getElementById(id);
-        imgs.push(img);
     }
-    return imgs;
 }
 
-checkSolution();
+function getAllImageIdsFromGameField() {
+    let imgIds = [];
+    for (i = 1; i < 12; i++) {
+        if (i < 10) {
+            var id = "#img-0".concat(i);
+        } else {
+            var id = "#img-".concat(i);
+        }
+        try {
+            let img = document.querySelector(id);
+            imgIds.push(img);
+        } catch (error) {
+            alert("No such image!");
+        }
+    }
+    return imgIds;
+}
+
+checkSolution(solution);
