493ff9cb24b44faa8ac06d61e21392b78d527f77
zoltanNemeth <nemeth.zoltan.elte@gmail.com>
2019-02-20 20:04:14 +0100

    TASK GENERATION next step

--- static/js/mentory.js
+++ static/js/mentory.js
@@ -75,27 +75,38 @@ function generateId(img, images) {
     return id
 }
 
-function getSolution() {
-    let solution = ["#img-01"];
-    return solution;
-}
-
 function checkSolution() {
-    let gameField = document.getElementById('game-field01');
-    gameField.addEventListener("drop", checkSolutionInner);
-}
 
-function checkSolutionInner(event) {
-    let imageIdsOnGameField = getAllImageIdsFromGameField(20);
-    let solution = getSolution();
-    for (index = 0; index < solution.length; index++) {
-        if (imageIdsOnGameField[index] != solution[index]) {
-            alert("Wrong solution... Maybe next time :D");
-            return
+    var solution = [];
+
+    let startButton = document.getElementById('start-game');
+    startButton.addEventListener("click", function(event) {
+        let newSolutionElementId = document.querySelector("#game-field-img-01")
+        solution.push(newSolutionElementId);
+    });
+
+    let nextLevelButton = document.getElementById('next-level');
+    nextLevelButton.addEventListener("click", function(event) {
+        clearGameField();
+        newFooter();
+        nextCardToShow();
+        let newSolutionElementId = document.querySelector("#game-field-img-01")
+        solution.push(newSolutionElementId);
+    });
+
+    let checkButton = document.getElementById('check-solution');
+    checkButton.addEventListener("click", function(event) {
+
+        let imageIdsOnGameField = getAllImageIdsFromGameField(20);
+
+        for (index = 0; index < solution.length; index++) {
+            if (imageIdsOnGameField[index] != solution[index]) {
+                alert("Wrong solution... Maybe next time :D");
+                main();
+            }
         }
-    }
-    alert("Correct.... But let's see the next one.")
-    return
+        alert("Correct.... But let's see the next one.")
+    });
 }
 
 function getAllImageIdsFromGameField(count) {
@@ -113,6 +124,43 @@ function getAllImageIdsFromGameField(count) {
         }
     }
     return imgIds;
+}
+
+function clearGameField() {
+    for (i = 1; i < 13; i++) {
+        if (i < 10) {
+            var id = "img-0".concat(i);
+        } else {
+            var id = "img-".concat(i);
+        }
+        let img = document.getElementById(id);
+        img.parentNode.removeChild(img);
+    }
+}
+
+function newFooter() {
+    let images = ["static/images/img_01.jpg", "static/images/img_02.jpg",
+                  "static/images/img_03.jpg", "static/images/img_04.jpg",
+                  "static/images/img_05.jpg", "static/images/img_06.jpg",
+                  "static/images/img_07.jpg", "static/images/img_08.jpg",
+                  "static/images/img_09.jpg", "static/images/img_10.jpg",
+                  "static/images/img_11.jpg", "static/images/img_12.jpg"];
+    for (i = 1; i < 13; i++) {
+        if (i < 10) {
+            var id = "img-0".concat(i);
+        } else {
+            var id = "img-".concat(i);
+        }
+        let img = document.createElement("img");
+        img.src = images[i];
+        img.id = id;
+        if (i < 10) {
+            var divId = "footer-field0".concat(i);
+        } else {
+            var divId = "footer-field".concat(i);
+        }
+        document.getElementById(divId).appendChild(img)
+    }
 }
 
 function main() {
