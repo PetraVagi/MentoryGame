7e294336a7ba8ae8ba478ed3bb07695fa54f3fbb
Lizi Boros <boroslizi@gmail.com>
2019-02-20 10:41:35 +0100

    "Drag and drop" feature works

--- static/js/mentory.js
+++ static/js/mentory.js
@@ -1,8 +1,21 @@
 function dragAndDrop() {
-    let libraryImages = document.getElementById('footer');
-    let gameImages = document.getElementById('game-field');
-    dragula([libraryImages, gameImages])
 
+    let libraryImages = document.getElementsByClassName('footer-library');
+    let gameImages = document.getElementsByClassName('game-library');
+
+    // defines containers argument for dragula to take
+    let containers = [];
+    for (let libraryImage of libraryImages) {
+        containers.push(libraryImage);
+    }
+    for (let gameImage of gameImages) {
+        containers.push(gameImage);
+    }
+
+
+    dragula(containers, {
+        copy: true
+    });
 }
 
 
