adcd3242c316b076940748c2eda6cadb495e3a31
Petrav <vagi.p94@gmail.com>
2019-02-21 11:47:29 +0100

    Merged check_order

--- static/js/mentory.js
+++ static/js/mentory.js
@@ -17,6 +17,85 @@ function dragAndDrop() {
     });
 }
 
+function nextCardToShow() {
+
+    let img = document.createElement("img");
+
+    let images = ["static/images/img_01.jpg", "static/images/img_02.jpg",
+                  "static/images/img_03.jpg", "static/images/img_04.jpg",
+                  "static/images/img_05.jpg", "static/images/img_06.jpg",
+                  "static/images/img_07.jpg", "static/images/img_08.jpg",
+                  "static/images/img_09.jpg", "static/images/img_10.jpg",
+                  "static/images/img_11.jpg", "static/images/img_12.jpg"];
+
+    img.src = getImageByRandomNumber(images);
+    img.id = generateId(img, images);
+
+    let div = document.getElementById("game-field01");
+
+
+
+
+    div.appendChild(img);
+
+    setTimeout(function(){
+    img.parentNode.removeChild(img);
+    }, 2000);
+}
+
+function getImageByRandomNumber(images) {
+
+let randomImg = Math.floor(Math.random() * 11);
+for (let i = 0; i <= images.length-1; i++) {
+
+        if (randomImg == i) {
+            return images[i]
+        }
+    }
+}
+
+function generateId(img, images) {
+
+
+    let id;
+    for (let i = 0; i <= images.length-1; i++) {
+
+        if(images[i] == img.getAttribute("src")) {
+            if(i < 9) {
+                let number = i+1;
+                id = "img-0" + number.toString();
+            }
+            else {
+                let number = i+1;
+                id = "img-" + number.toString();
+            }
+        }
+    }
+    return id
+}
+
+function clearSlots() {
+
+    let nextLevelButton = document.getElementById('next-level');
+    let gameSlots = document.getElementsByClassName('game-library');
+
+    nextLevelButton.addEventListener('click', function () {
+        for (let slot of gameSlots) {
+            while (slot.hasChildNodes()) {
+                slot.removeChild(slot.firstChild);
+            }
+        }
+    });
+}
+
+function retryLevel() {
+
+    let retryButton = document.getElementById('retry-button');
+
+    retryButton.addEventListener('click', function () {
+        window.location.href = '/';
+    });
+}
 
 function countScore() {
     const test = document.createElement('button');
@@ -42,7 +121,10 @@ function TEST() {
 
 
 function main() {
+    retryLevel();
     dragAndDrop();
+    nextCardToShow();
+    clearSlots();
     countScore()
 }
 
