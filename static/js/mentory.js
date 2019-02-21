1ab52d95ca69daa201b5f7450dcba50459b229a2
Petrav <vagi.p94@gmail.com>
2019-02-21 09:39:30 +0100

    Score trying (not working)

--- static/js/mentory.js
+++ static/js/mentory.js
@@ -18,9 +18,28 @@ function dragAndDrop() {
 }
 
 
+function countScore() {
+    let startButton = document.getElementById('start');
+    startButton.addEventListener('click', function () {
+        startButton.classList.toggle('clicked')
+    })
+}
+
+
+var startScore = 1000;
+var score = document.getElementById('score');
+
+function Score() {
+    startScore -= 1;
+    score.innerHTML = "Score: " + startScore;
+}
+
+var interval = window.setInterval(Score, 1000);
+
 
 function main() {
     dragAndDrop();
+    countScore()
 }
 
 
